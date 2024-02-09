8f7040a88047603e7682383b65089ab1b73f66be
tandotbt <tan.dot.bt@gmail.com>
2024-02-09 10:37:03 +0700

    🔨 refactor: đổi link nhận block
    - do nhận liên tục nên cái server không chịu được nhiệt, thôi thì quay lại dùng link jsonblob :v
    - bảo mật sẽ không còn

--- js/heimdall/const.js
+++ js/heimdall/const.js
@@ -3,9 +3,9 @@ const SERVER_PLANET_USE = "heimdall"
 const url_9capi_sim = "https://api.9capi.com/arenaSimHeimdall";
 const URL_9CSCAN = "https://api-heimdall.9cscan.com";
 const url_9capi_leadboard = "https://api.9capi.com/arenaLeaderboardHeimdall";
-const url_jsonblod_data_arena_2 = `${URL_API_9CMD}/data_9cmd_arena?serverPlanet=${SERVER_PLANET_USE}&type=url_jsonblod_data_arena_2`;
+const url_jsonblod_data_arena_2 = "https://jsonblob.com/api/1194537739986264064";
 const url_jsonblod_leadboard_2 = "https://jsonblob.com/api/1194537894173073408";
-const url_jsonblod_time_block = `${URL_API_9CMD}/block?serverPlanet=${SERVER_PLANET_USE}`;
+const url_jsonblod_time_block = "https://jsonblob.com/api/1194552651059945472";
 const url_jsonblod_message = "https://jsonblob.com/api/1193628388912128000";
 const urls_node = [
   "https://heimdall-rpc-1.nine-chronicles.com/graphql",
--- js/odin/const.js
+++ js/odin/const.js
@@ -3,9 +3,9 @@ const SERVER_PLANET_USE = "odin"
 const url_9capi_sim = "https://api.9capi.com/arenaSimOdin";
 const URL_9CSCAN = "https://api.9cscan.com";
 const url_9capi_leadboard = "https://api.9capi.com/arenaLeaderboardOdin";
-const url_jsonblod_data_arena_2 = `${URL_API_9CMD}/data_9cmd_arena?serverPlanet=${SERVER_PLANET_USE}&type=url_jsonblod_data_arena_2`;
+const url_jsonblod_data_arena_2 = "https://jsonblob.com/api/1194521453235396608";
 const url_jsonblod_leadboard_2 = "https://jsonblob.com/api/1194537532422742016";
-const url_jsonblod_time_block = `${URL_API_9CMD}/block?serverPlanet=${SERVER_PLANET_USE}`;
+const url_jsonblod_time_block = "https://jsonblob.com/api/1194552728394522624";
 const url_jsonblod_message = "https://jsonblob.com/api/1193642586002022400";
 const urls_node = [
   "https://odin-rpc-1.nine-chronicles.com/graphql",
